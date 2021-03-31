5407781e51d1718c3806125226581764f9d5f981
Dan Radu <dan.radu@msg.group>
2021-03-31 10:15:57 +0300

    Web components support

--- src/web-component-wrapper.js
+++ src/web-component-wrapper.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import wrap from '@vue/web-component-wrapper';
 import VueWebComponent from './components/VueWebComponent';
 
+Vue.config.productionTip = false;
+Vue.config.devtools = false;
+
 const CustomElement = wrap(Vue, VueWebComponent);
 
 window.customElements.define('my-custom-element', CustomElement);
