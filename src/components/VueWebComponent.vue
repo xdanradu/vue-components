3e5cf18bed4dee25fc4b16f2532b6282adda628b
Dan Radu <dan.radu@msg.group>
2021-03-31 14:27:45 +0300

    Web components support

--- src/components/VueWebComponent.vue
+++ src/components/VueWebComponent.vue
@@ -1,11 +1,32 @@
 <template>
-    <div>
+    <div class="card">
         <h1>My Vue Web Component</h1>
-        <div>{{ msg }}</div>
+        <div @click="onClickButton($event)" class="message">{{ msg }}</div>
     </div>
 </template>
 <script>
     export default {
-        props: ['msg']
+        emits: ['divclicked'],
+        props: ['msg'],
+        methods: {
+            onClickButton () {
+                console.log('clicked in child');
+                const message = 'some message from the component';
+                this.$emit('divclicked', 'submit', { message })
+            }
+        }
     }
 </script>
+
+<style scoped>
+    .card {
+        font-family: 'Roboto', sans-serif;
+        color: crimson;
+        text-underline: crimson;
+    }
+    .message {
+        background-color: cornflowerblue;
+        color: white;
+    }
+
+</style>
